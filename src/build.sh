f5f0d0fe74bb959cf21c7b5be6a25264954ca5b9
Pedro Netto <pedro@pnetto.com>
2026-01-02 21:19:47 +0100

    Update deploy flow

--- src/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-if [[ "$(docker images -q demo-garden-image 2> /dev/null)" == "" ]]; then
-  echo "Building Base VM image..."
-  docker build -t demo-garden-image .
-else
-  echo "Base VM image already exists. Skipping build."
-fi
